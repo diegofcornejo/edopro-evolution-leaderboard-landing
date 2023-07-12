021cd0313b87d2a7a71c9400635059da2e8b88a5
Diego Cornejo <diegof.cornejo@gmail.com>
2023-07-12 09:36:37 -0600

    chore: Get leaderboard using redis instead api

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { createClient } from 'redis';
+const client = createClient({ url: process.env.REDIS_URL });
+client.on('error', (err) => console.log('Redis Client Error', err));
+
 import Banner from './components/Banner/index';
 import Companies from './components/Companies/index';
 import Work from './components/Work/index';
@@ -7,26 +11,30 @@ import Simple from './components/Simple/index';
 import Trade from './components/Trade/index';
 import Faq from './components/Faq/index';
 
-
 async function getLeaderboard() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/leaderboard`)
-    const leaderboard = await res.json()
-    return leaderboard
-  }
+	// const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/leaderboard`)
+	// const leaderboard = await res.json()
+	// return leaderboard
+	await client.connect();
+	let leaderboard = await client.ZRANGEBYSCORE_WITHSCORES('leaderboard', '-inf', '+inf');
+	leaderboard = leaderboard.sort((a, b) => b.score - a.score);
+	await client.quit();
+	return leaderboard;
+}
 
 export default async function Home() {
-  const leaderboard = await getLeaderboard();  
-  console.log(leaderboard);
-  return (
-    <main>
-      <Banner />
-      <Companies />
-      <Work leaderboard={leaderboard}/>
-      <Table leaderboard={leaderboard}/>
-      <Features />
-      <Simple />
-      <Trade />
-      <Faq />
-    </main>
-  )
+	const leaderboard = await getLeaderboard();
+	console.log(leaderboard);
+	return (
+		<main>
+			<Banner />
+			<Companies />
+			<Work leaderboard={leaderboard} />
+			<Table leaderboard={leaderboard} />
+			<Features />
+			<Simple />
+			<Trade />
+			<Faq />
+		</main>
+	);
 }
