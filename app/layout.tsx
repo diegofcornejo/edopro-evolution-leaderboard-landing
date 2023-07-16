37ff3fa7bc13fdd9d153699088bf010cf407cbb2
Diego Cornejo <diegof.cornejo@gmail.com>
2023-07-15 21:38:30 -0600

    Added umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,28 +2,27 @@ import './globals.css';
 import '@/node_modules/react-modal-video/scss/modal-video.scss';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
-
+import Script from 'next/script'
 
 export const metadata = {
-  title: 'Evolution - EDOPro Server',
-  description: 'Leaderboard for Evolution EDOPro Server',
-  icons: {
-    icon: '/logo.svg',
-  },
-}
+	title: 'Evolution - EDOPro Server',
+	description: 'Leaderboard for Evolution EDOPro Server',
+	icons: {
+		icon: '/logo.svg',
+	},
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en'>
+			<head>
+				<Script src="https://las-analytics.vercel.app/script.js" data-website-id="d49d0638-0710-4b43-91c8-e887c1afb079" />
+			</head>
+			<body>
+				<Navbar />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
