ddff068cdb92ba9bb3f1519dd39e8f52b503d3d7
Diego Cornejo <diegof.cornejo@gmail.com>
2023-07-19 21:53:52 -0600

    feat: Added support for onesignal web notifications

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,44 +1,46 @@
-"use client"
+'use client';
 import Navbar from './Navbar';
 import React, { useEffect } from 'react';
+import runOneSignal from '../../libs/onesignal';
 
 const Navbarin: React.FC = () => {
-    useEffect(() => {
-        // The debounce function receives our function as a parameter
-        const debounce = (fn: Function) => {
-            // This holds the requestAnimationFrame reference, so we can cancel it if we wish
-            let frame: number;
-            // The debounce function returns a new function that can receive a variable number of arguments
-            return (...params: any[]) => {
-                // If the frame variable has been defined, clear it now, and queue for next frame
-                if (frame) {
-                    cancelAnimationFrame(frame);
-                }
-                // Queue our function call for the next frame
-                frame = requestAnimationFrame(() => {
-                    // Call our function and pass any params we received
-                    fn(...params);
-                });
-            }
-        };
+	useEffect(() => {
+		runOneSignal();
+		// The debounce function receives our function as a parameter
+		const debounce = (fn: Function) => {
+			// This holds the requestAnimationFrame reference, so we can cancel it if we wish
+			let frame: number;
+			// The debounce function returns a new function that can receive a variable number of arguments
+			return (...params: any[]) => {
+				// If the frame variable has been defined, clear it now, and queue for next frame
+				if (frame) {
+					cancelAnimationFrame(frame);
+				}
+				// Queue our function call for the next frame
+				frame = requestAnimationFrame(() => {
+					// Call our function and pass any params we received
+					fn(...params);
+				});
+			};
+		};
 
-        // Reads out the scroll position and stores it in the data attribute
-        // so we can use it in our stylesheets
-        const storeScroll = () => {
-            document.documentElement.dataset.scroll = window.scrollY.toString();
-        }
+		// Reads out the scroll position and stores it in the data attribute
+		// so we can use it in our stylesheets
+		const storeScroll = () => {
+			document.documentElement.dataset.scroll = window.scrollY.toString();
+		};
 
-        // Listen for new scroll events, here we debounce our `storeScroll` function
-        document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+		// Listen for new scroll events, here we debounce our `storeScroll` function
+		document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
-        // Update scroll position for first time
-        storeScroll();
-    }, [])
-    return (
-        <>
-            <Navbar />
-        </>
-    );
-}
+		// Update scroll position for first time
+		storeScroll();
+	}, []);
+	return (
+		<>
+			<Navbar />
+		</>
+	);
+};
 
 export default Navbarin;
