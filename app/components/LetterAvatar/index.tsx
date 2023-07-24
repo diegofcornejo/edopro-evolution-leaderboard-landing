3655d3d355092bd4ed4cdf4efc3443ee7400dadb
Diego Cornejo <diegof.cornejo@gmail.com>
2023-07-24 17:35:23 -0600

    chore: Minor styles changed

--- app/components/LetterAvatar/index.tsx
+++ app/components/LetterAvatar/index.tsx
@@ -2,14 +2,24 @@
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 
-
-const LetterAvatar = ({name, size, borderColor='#291545'}) => {
+const LetterAvatar = ({ name, size, borderColor = '#291545', fontSize = '1.25rem' }) => {
 	const letters = name.slice(0, 2).toUpperCase();
 	return (
-    <Stack direction="row" spacing={2}>
-      <Avatar sx={{ bgcolor: '#291545', width: size, height: size, borderColor, borderWidth: '1px'}}>{letters}</Avatar>
-    </Stack>
-  );
+		<Stack direction='row' spacing={2}>
+			<Avatar
+				sx={{
+					bgcolor: '#291545',
+					width: size,
+					height: size,
+					borderColor,
+					borderWidth: '1px',
+					fontSize,
+				}}
+			>
+				{letters}
+			</Avatar>
+		</Stack>
+	);
 };
 
 export default LetterAvatar;
--- app/components/Table/index.tsx
+++ app/components/Table/index.tsx
@@ -13,7 +13,7 @@ const Table = ({ leaderboard }) => {
 						<thead>
 							<tr className='text-white bg-darkblue rounded-lg'>
 								<th className='px-4 py-4 font-normal'>RANK</th>
-								<th className='px-4 py-4 text-start font-normal'>USER</th>
+								<th className='px-4 py-4 text-start font-normal'>USERNAME</th>
 								<th className='px-4 py-4 font-normal'>POINTS</th>
 								<th className='px-4 py-4 font-normal'>WINS</th>
 								<th className='px-4 py-4 font-normal'>LOSSES</th>
@@ -23,22 +23,26 @@ const Table = ({ leaderboard }) => {
 						<tbody>
 							{leaderboard.map((items, i) => (
 								<tr key={i} className='border-b border-b-darkblue'>
-									<td className='px-4 py-4 text-center text-white'>{i+4}</td>
-									<td className='px-4 py-4 text-center text-white flex items-center justify-start gap-5 '>
-										<LetterAvatar name={items.value} size={56}/>
+									<td className='px-4 py-2 text-center text-white'>{i + 4}</td>
+									<td className='px-4 py-2 text-center text-white flex items-center justify-start gap-5 '>
+										<LetterAvatar
+											name={items.value}
+											size={40}
+											fontSize={`1rem`}
+										/>
 										{items.value}
 									</td>
-									
-									<td className='px-4 py-4 text-center text-white'>
+
+									<td className='px-4 py-2 text-center text-white'>
 										{items.score.toLocaleString()}
 									</td>
-									<td className='px-4 py-4 text-center text-green'>
+									<td className='px-4 py-2 text-center text-green'>
 										{items.wins.toLocaleString()}
 									</td>
-									<td className='px-4 py-4 text-center text-red'>
-                                        {items.losses.toLocaleString()}
+									<td className='px-4 py-2 text-center text-red'>
+										{items.losses.toLocaleString()}
 									</td>
-									<td className='px-4 py-4 text-center text-white'>
+									<td className='px-4 py-2 text-center text-white'>
 										{items.winrate}%
 									</td>
 								</tr>
@@ -48,7 +52,7 @@ const Table = ({ leaderboard }) => {
 				</div>
 			</div>
 			<Image
-				src={'/images/Table/Untitled.svg'}
+				src={'/images/Table/Footer.svg'}
 				alt='ellipse'
 				width={2460}
 				height={102}
