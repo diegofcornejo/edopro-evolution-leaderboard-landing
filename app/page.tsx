14e2f5c6185d5c24c75b420c60aead932c292410
Diego Cornejo <diegof.cornejo@gmail.com>
2023-08-12 13:50:49 -0600

    feat: Show live games in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from 'react-hot-toast';
 import Banner from './components/Banner/index';
 // import Companies from './components/Companies/index';
+import Live from './components/Live/index';
 import Top from './components/Top/index';
 import Table from './components/Table/index';
 import Features from './components/Features/index';
@@ -15,12 +16,20 @@ const getLeaderBoard = async () => {
 	return data;
 }
 
+const getRooms = async () => {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/rooms`,{ cache: 'no-store' });
+	const data = await res.json();
+	return data;
+}
+
 export default async function Home() {
+	const rooms = await getRooms();
 	const leaderboard = await getLeaderBoard();
 	return (
 		<main>
 			<Banner />
 			{/* <Companies /> */}
+			<Live rooms={rooms}/>
 			<Top leaderboard={leaderboard}/>
 			<Table ranking={leaderboard} />
 			<Features />
