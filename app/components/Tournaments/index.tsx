23668a147921353985f1ce7d03ce819526337346
Diego Cornejo <diegof.cornejo@gmail.com>
2023-08-11 20:10:54 -0600

    chore: Updated tournament create options

--- app/components/Tournaments/index.tsx
+++ app/components/Tournaments/index.tsx
@@ -47,15 +47,12 @@ const Table = ({ tournaments }) => {
 							<tr className='text-white bg-darkblue rounded-lg'>
 								<th className='px-4 py-4 text-start font-normal'></th>
 								<th className='px-4 py-4 text-start font-normal'>NAME</th>
-								{/* <th className='px-4 py-4 text-start font-normal'>TYPE</th> */}
-								{/* <th className='px-4 py-4 font-normal'>CREATOR</th> */}
-								<th className='px-4 py-4 text-start font-normal'>STATUS</th>
-								<th className='px-4 py-4 text-start font-normal'>START</th>
-								{/* <th className='px-4 py-4 font-normal'>END</th> */}
-								<th className='px-4 py-4 text-start font-normal'>BANLIST</th>
-								<th className='px-4 py-4 text-start font-normal'>MODE</th>
+								<th className='px-4 py-4 font-normal'>MODE</th>
 								<th className='px-4 py-4 font-normal'>BESTOF</th>
-								{/* <th className='px-4 py-4 font-normal'>RULES</th> */}
+								<th className='px-4 py-4 text-start font-normal'>BANLIST</th>
+								<th className='px-4 py-4 text-start font-normal'>MASTER RULE</th>
+								{/* <th className='px-4 py-4 text-start font-normal'>STATUS</th> */}
+								<th className='px-4 py-4 text-start font-normal'>START</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -67,34 +64,16 @@ const Table = ({ tournaments }) => {
 								>
 									<td className='px-4 py-2 text-center text-white'>{i + 1}</td>
 									<td className='px-4 py-2 text-white'>{items.name}</td>
-									{/* <td className='px-4 py-2 text-white'>
-										{items.type}
-									</td> */}
-									{/* <td className='px-4 py-2 text-center text-white flex items-center justify-start gap-5 '>
-										{items.avatar ? (
-											<UserAvatar size={'40px'} avatarParts={items.avatar} />
-										) : (
-											<LetterAvatar
-												name={items.creator}
-												size={40}
-												fontSize={`1rem`}
-											/>
-										)}
-										{items.creator}
-									</td> */}
-									<td className='px-4 py-2 text-white'>{items.status}</td>
-									<td className='px-4 py-2 text-white'>{items.startDate}</td>
-									{/* <td className='px-4 py-2 text-center text-white'>
-										{items.end}
-									</td> */}
-									<td className='px-4 py-2 text-white'>{items.banlist}</td>
-									<td className='px-4 py-2 text-white'>{items.mode}</td>
+									<td className='px-4 py-2 text-center text-white'>
+										{items.mode} v {items.mode}
+									</td>
 									<td className='px-4 py-2 text-center text-white'>
 										{items.bestOf}
 									</td>
-									{/* <td className='px-4 py-2 text-center text-white'>
-										{items.rules}
-									</td> */}
+									<td className='px-4 py-2 text-white'>{items.banlist}</td>
+									<td className='px-4 py-2 text-white'>{items.rule}</td>
+									{/* <td className='px-4 py-2 text-white'>{items.status}</td> */}
+									<td className='px-4 py-2 text-white'>{items.startDate}</td>
 								</tr>
 							))}
 						</tbody>
