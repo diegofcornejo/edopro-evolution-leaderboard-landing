54d514b48377bb94f1427e5ebe071339e8e0c934
Diego Cornejo <diegof.cornejo@gmail.com>
2023-09-13 09:08:08 -0600

    fix: Load analytics scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='en'>
 			<head>
-				<Script src="https://las-analytics.vercel.app/script.js" data-website-id="d49d0638-0710-4b43-91c8-e887c1afb079" />
-				<Script type="text/javascript">
-					(function(c,l,a,r,i,t,y){
-						c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    			})(window, document, "clarity", "script", "iukdbfryb2");
-				</Script>
+				<Script id="umami-script" src="https://las-analytics.vercel.app/script.js" data-website-id="d49d0638-0710-4b43-91c8-e887c1afb079" strategy="beforeInteractive"/>
+				<Script type="text/javascript" id="clarity-script">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script","iukdbfryb2");
+          `}
+        </Script>
 			</head>
 			<body>
 				<Navbar />
