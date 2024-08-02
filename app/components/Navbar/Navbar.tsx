e8b152ab87487d8e08df7547e28e84c4fb2d3340
Diego Cornejo <diegof.cornejo@gmail.com>
2024-08-02 07:10:45 -0600

    refactor: Menu items

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -10,53 +10,7 @@ import Profile from './Profile';
 import SendMessageDialog from './SendMessageDialog';
 import { AuthContext } from '@/context/auth/AuthContext';
 // import Contactusform from './Contactus';
-
-interface NavigationItem {
-	name: string;
-	href: string;
-	current: boolean;
-	target?: string;
-	show?: boolean;
-	permission?: string[];
-}
-
-const navigation: NavigationItem[] = [
-	{ name: 'Home', href: '/#home-section', current: false, show: true, permission: ['ANY'] },
-	// { name: 'Top', href: '#topplayers-section', current: false },
-	{ name: 'Ranking', href: '/#topplayers-section', current: false, show: false, permission: ['ANY'] },
-	{ name: 'Features', href: '/#features-section', current: false, show: false, permission: ['ANY'] },
-	{ name: 'Download', href: '/#download-section', current: false, show: false, permission: ['ANY'] },
-	{ name: 'Tournaments', href: '/tournaments', current: false, show: false, permission: ['ANY'] },
-	{ name: 'Banlists', href: '/banlists', current: false, show: false, permission: ['MANAGER','ADMIN'] },
-	{
-		name: 'Status',
-		href: 'https://status.evolutionygo.com',
-		current: false,
-		target: '_blank',
-		show: false,
-		permission: ['ANY']
-	}
-	// {
-	// 	name: 'Github',
-	// 	href: 'https://github.com/diangogav/EDOpro-server-ts',
-	// 	current: false,
-	// 	target: '_blank',
-	// }
-];
-
-// get menu items based on user permissions
-const getNavigation = (user) => {
-	const permissions = user?.role;
-	const nav = navigation.map((item) => {
-		if (item.permission.includes('ANY')) {
-			item.show = true;
-		} else if (item.permission.includes(permissions)) {
-			item.show = true;
-		}
-		return item;
-	});
-	return nav;
-}
+import { getNavigation } from './menuItems';
 
 function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(' ');
