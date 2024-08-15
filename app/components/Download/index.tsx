9714165f58f15d528fd16a3eb30a0a0fef6d0eb4
Diego Cornejo <diegof.cornejo@gmail.com>
2024-08-15 10:57:49 -0600

    chore: add supported clients buttons

--- app/components/Download/index.tsx
+++ app/components/Download/index.tsx
@@ -1,5 +1,33 @@
 import Link from 'next/link';
 
+type Client = {
+	name: string;
+	url: string;
+};
+
+const clients: Client[] = [
+	{
+		name: 'Koishi Pro',
+		url: 'https://koishi.pro/download/',
+	},
+	{
+		name: 'YGO Mobile',
+		url: 'https://ygom.top/',
+	},
+	{
+		name: 'EDOPro (Recommended)',
+		url: 'https://projectignis.github.io/download.html',
+	},
+	{
+		name: 'MDPro3 Windows',
+		url: 'https://www.mediafire.com/folder/mxhs3xpc26nme/Window',
+	},
+	{
+		name: 'MDPro3 Android',
+		url: 'https://www.mediafire.com/folder/aa8cr3joj1evv/Android',
+	}
+];
+
 const Download = () => {
 	return (
 		<div className='simple-bg relative' id='download-section'>
@@ -14,14 +42,21 @@ const Download = () => {
 				</h3>
 				<p className='text-center text-bluish text-lg font-normal mb-8'>
 					Evolution does not have its own client. <br />
-					We recommend downloading the official EDOPro client.
+					We recommend downloading the official EDOPro client or any of the other clients listed below.
 				</p>
 				<div className='flex justify-center '>
-					<Link href='https://projectignis.github.io/download.html' rel="noopener noreferrer" target="_blank">
+					{clients.map((client) => (
+						<Link href={client.url} key={client.name} rel="noopener noreferrer" target="_blank">
+							<button className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton mr-4'>
+								{client.name}
+							</button>
+						</Link>
+					))}
+					{/* <Link href='https://projectignis.github.io/download.html' rel="noopener noreferrer" target="_blank">
 						<button className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton'>
 							Download
 						</button>
-					</Link>
+					</Link> */}
 				</div>
 			</div>
 			<div className='simplesix'></div>
--- app/components/Faq/index.tsx
+++ app/components/Faq/index.tsx
@@ -2,72 +2,89 @@
 import Image from "next/image";
 import { Disclosure } from '@headlessui/react';
 import { ChevronUpIcon } from '@heroicons/react/20/solid';
+import Link from "next/link";
 
-interface faqdata {
-    heading: string;
-    subheading: string;
+interface FaqData {
+	heading: string;
+	subheading: React.ReactNode[];
 }
 
-const faqdata: faqdata[] = [
-    {
-        heading: "1. How to play on Evolution Server?",
-        subheading: `You only need to download a compatible client. We recommend downloading the official EDOPro client (Project Ignis) or from the downloads section on our Discord server. Finally, follow the instructions provided on our Discord to set up the server.`
-    },
-    {
-        heading: "2. Do I need an account to play on Evolution Server?",
-        subheading: `You don't need an account to play on Evolution Server, but for a complete experience, we recommend registering. In addition to automatic ranking you'll gain access to more content and special events.`
-    },
-    {
-        heading: "3. How does ranking work?",
-        subheading: 'No matter which game mode you play, you will earn points for each victory. The more points you accumulate, the higher your rank will be. You can check your rank on the website; the website only displays the top 20 ranks. If you are not in the top 20, you can find your rank in your profile.'
-    },
-
-]
+const faqdata: FaqData[] = [
+	{
+		heading: "1. How to play on Evolution Server?",
+		subheading: [
+			`You only need to download a compatible client. We recommend downloading the official EDOPro client (Project Ignis) or any of the other clients listed on the download section. Finally, follow the instructions provided on our `,
+			<Link href="https://discord.com/invite/f6UkbRyqUz" key="link">
+				<span className="text-red">Discord</span>
+			</Link>,
+			` to setup the server.`,
+		],
+	},
+	{
+		heading: "2. Do I need an account to play on Evolution Server?",
+		subheading: [
+			`You don't need an account to play on Evolution Server, but for a complete experience, we recommend registering. In addition to automatic ranking you'll gain access to more content and special events.`,
+		],
+	},
+	{
+		heading: "3. How does ranking work?",
+		subheading: [
+			"No matter which game mode you play, you will earn points for each victory. The more points you accumulate, the higher your rank will be. You can check your rank on the website; the website only displays the top 20 ranks. If you are not in the top 20, you can find your rank in your profile.",
+		],
+	},
+];
 
 const Faq = () => {
-    return (
-        <div className="my-20 px-6" id="faq-section">
-            <h3 className="text-center text-3xl lg:text-5xl font-bold text-offwhite mb-3">Frequently Asked And Question</h3>
-            <p className="text-center lg:text-lg font-normal text-bluish">New to Evolution Server? Here is a list of common questions that you can review.<br/>If you have any other questions, feel free to communicate with us on our Discord</p>
+	return (
+		<div className="my-20 px-6" id="faq-section">
+			<h3 className="text-center text-3xl lg:text-5xl font-bold text-offwhite mb-3">Frequently Asked And Question</h3>
+			<p className="text-center lg:text-lg font-normal text-bluish">New to Evolution Server? Here is a list of common questions that you can review.<br />If you have any other questions, feel free to communicate with us on our Discord</p>
 
-            <div className="mx-auto max-w-7xl">
-                <div className="grid lg:grid-cols-2">
-                    {/* Column-1 */}
-                    <div>
-                        <div className="w-full px-4 pt-16">
+			<div className="mx-auto max-w-7xl">
+				<div className="grid lg:grid-cols-2">
+					{/* Column-1 */}
+					<div>
+						<div className="w-full px-4 pt-16">
 
-                            {faqdata.map((items, i) => (
-                                <div className="mx-auto w-full max-w-5xl rounded-2xl bg-blue py-8 px-6 mb-5" key={i}>
-                                    <Disclosure>
-                                        {({ open }) => (
-                                            <>
-                                                <Disclosure.Button className="flex w-full justify-between rounded-lg text-offwhite sm:px-4 sm:py-2 text-left md:text-2xl font-medium">
-                                                    <span>{items.heading}</span>
-                                                    <ChevronUpIcon
-                                                        className={`${open ? 'rotate-180 transform' : ''
-                                                            } h-5 w-5 text-purple-500`}
-                                                    />
-                                                </Disclosure.Button>
-                                                <Disclosure.Panel className="px-4 pt-4 pb-2 md:text-lg text-bluish font-normal opacity-50">{items.subheading}</Disclosure.Panel>
-                                            </>
-                                        )}
-                                    </Disclosure>
-                                </div>
-                            ))}
+							{faqdata.map((items, i) => (
+								<div
+									className="mx-auto w-full max-w-5xl rounded-2xl bg-blue py-8 px-6 mb-5"
+									key={i}
+								>
+									<Disclosure>
+										{({ open }) => (
+											<>
+												<Disclosure.Button className="flex w-full justify-between rounded-lg text-offwhite sm:px-4 sm:py-2 text-left md:text-2xl font-medium">
+													<span>{items.heading}</span>
+													<ChevronUpIcon
+														className={`${open ? "rotate-180 transform" : ""
+															} h-5 w-5 text-purple-500`}
+													/>
+												</Disclosure.Button>
+												<Disclosure.Panel className="px-4 pt-4 pb-2 md:text-lg text-bluish font-normal opacity-50">
+													{items.subheading.map((element, index) => (
+														<span key={index}>{element}</span>
+													))}
+												</Disclosure.Panel>
+											</>
+										)}
+									</Disclosure>
+								</div>
+							))}
 
-                        </div>
-                    </div>
+						</div>
+					</div>
 
-                    {/* Column-2 */}
-                    <div className="mt-32">
-                        <Image src={'/images/Faq/faq.svg'} alt="faq-image" width={941} height={379} />
-                    </div>
+					{/* Column-2 */}
+					<div className="mt-32">
+						<Image src={'/images/Faq/faq.svg'} alt="faq-image" width={941} height={379} />
+					</div>
 
-                </div>
-            </div>
+				</div>
+			</div>
 
-        </div>
-    )
+		</div>
+	)
 }
 
 export default Faq;
