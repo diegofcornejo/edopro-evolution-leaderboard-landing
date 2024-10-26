a130749623a3f3d6bd9aefb77e9678aee9fdfea5
Diego Cornejo <diegof.cornejo@gmail.com>
2024-10-25 21:40:08 -0600

    chore: tmp api tournaments

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -2,9 +2,10 @@ import { Toaster } from 'react-hot-toast';
 import Tournaments from '../components/Tournaments/index';
 
 const getTournaments = async () => {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tournament`,{ cache: 'no-store' });
-	const data = await res.json();
-	return data;
+	// const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tournament`,{ cache: 'no-store' });
+	// const data = await res.json();
+	// return data;
+	return [];
 }
 
 export default async function Home() {
--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"type": "postgres",
-	"host": "localhost",
-	"port": 5432,
+	"host": process.env.POSTGRES_HOST,
+	"port": process.env.POSTGRES_PORT,
 	"username": process.env.POSTGRES_USER,
 	"password": process.env.POSTGRES_PASSWORD,
 	"database": process.env.POSTGRES_DB,
